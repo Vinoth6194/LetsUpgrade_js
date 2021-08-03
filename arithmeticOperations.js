185a5ac192c64a01ff72fc46088c525d7e34872b
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-08-03 17:23:58 +0530

    arithmetic operations -- assignment 1

---
+++ arithmeticOperations.js
@@ -0,0 +1,8 @@
+var num1 = 30;
+var num2 = 20;
+
+console.log(`Addition of ${num1} and ${num2} is ${num1 + num2}`);
+console.log(`Subtraction of ${num1} and ${num2} is ${num1 - num2}`);
+console.log(`Multiplication of ${num1} and ${num2} is ${num1 * num2}`);
+console.log(`Division of ${num1} and ${num2} is ${num1 / num2}`);
+console.log(`Modulus of ${num1} and ${num2} is ${num1 % num2}`);
